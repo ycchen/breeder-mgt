09077fa9c27ba1ef517775249162839750c6a22c
Yungchih Chen <yungchih_chen@yahoo.com>
2013-08-29 18:16:20 -0400

    Added total records information for each models

--- app/models/pregnancy.rb
+++ app/models/pregnancy.rb
@@ -5,4 +5,6 @@ class Pregnancy < ActiveRecord::Base
 
   validates :dog_id, presence: true
   validates :total_puppy, presence: true, numericality: true
+
+  scope :expected, lambda{where('surgery_date >= ?', Date.today.to_s)}
 end

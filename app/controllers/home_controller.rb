9062e0bd15659692d71b18aed01df905054b3e62
Yungchih Chen <yungchih_chen@yahoo.com>
2013-10-11 11:10:46 -0400

    Fixed group by clause on home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   		@pregnancy = Pregnancy.already_due.order('due_date')
   		@colors = Charge.all.map{|c| ["##{rand(0xffffff).to_s(16)}"]}
 		@puppy_by_color =  Color.all.map{|c| ["#{c.name}", c.litters.size]}.to_s.html_safe
-		@puppy_by_gender = Litter.select('count(*) as gcount, gender, colors.name').joins(:color).where(survival: true).group('litters.gender, litters.color_id').map{|g| ["#{g.name}-#{g.gender}", g.gcount]} 
+		@puppy_by_gender = Litter.select('count(*) as gcount, gender, colors.name').joins(:color).where(survival: true).group('litters.gender, colors.name').map{|g| ["#{g.name}-#{g.gender}", g.gcount]} 
 		# @puppy_data = Litter.joins(:color).group(:name).size.map{ |l| l}
 		@litter_bitches = @pregnancy.map{|x| x.dog.call_name}.to_s.html_safe 
 

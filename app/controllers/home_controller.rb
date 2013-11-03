a86b88c74aa0d2eb44c6586c4323d233ff3e80de
Yungchih Chen <yungchih_chen@yahoo.com>
2013-11-03 10:24:41 -0500

    Modified pregnancy index to add more information about litter, and litter editing screen to add dog_id field

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
   		# redirect_to pregnancies_path
   		@pregnancy = Pregnancy.already_due.order('due_date')
   		@colors = Charge.all.map{|c| ["##{rand(0xffffff).to_s(16)}"]}
-		@puppy_by_color =  Color.all.map{|c| ["#{c.name}", c.litters.size]}.to_s.html_safe
-		@puppy_by_gender = Litter.select('count(*) as gcount, gender, colors.name').joins(:color).where(survival: true).group('litters.gender, colors.name').map{|g| ["#{g.name}-#{g.gender}", g.gcount]} 
-		# @puppy_data = Litter.joins(:color).group(:name).size.map{ |l| l}
-		@litter_bitches = @pregnancy.map{|x| x.dog.call_name}.to_s.html_safe 
+  		@puppy_by_color =  Color.all.map{|c| ["#{c.name}", c.litters.size]}.to_s.html_safe
+  		@puppy_by_gender = Litter.select('count(*) as gcount, gender, colors.name').joins(:color).where(survival: true).group('litters.gender, colors.name').map{|g| ["#{g.name}-#{g.gender}", g.gcount]} 
+  		# @puppy_data = Litter.joins(:color).group(:name).size.map{ |l| l}
+  		@litter_bitches = @pregnancy.map{|x| x.dog.call_name}.to_s.html_safe 
 
   	end
   end

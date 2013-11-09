e98ff25f1e2a64f5873869d9d3102353778f94d0
Yungchih Chen <yungchih_chen@yahoo.com>
2013-11-09 09:21:12 -0500

    Added table-condensed to all tables in view and updated sale order by sale_date desc

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -4,7 +4,7 @@ class SalesController < ApplicationController
   # GET /sales
   # GET /sales.json
   def index
-    @sales = Sale.all
+    @sales = Sale.order(sale_date: :desc)
   end
 
   # GET /sales/1
